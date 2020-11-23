11f8079356a64aceebe8b01c56cab31a10924a30
Adam Tindale <adam@adamtindale.com>
2020-11-23 12:11:02 -0500

    Update demo.js
    Adding nexusui.js component.

--- demo.js
+++ demo.js
@@ -13,6 +13,9 @@ fluid.defaults("flock.demo.oscilOscil", {
 
         synth: {
             type: "flock.demo.oscilOscil.synth"
+        },
+        nexusui: {
+            type: "flock.demo.oscilOscil.nexusui"
         }
     }
 });
@@ -42,6 +45,24 @@ fluid.defaults("flock.demo.oscilOscil.synth", {
                 rate: "control",
                 freq: 1,
                 phase: 1.0
+            }
+        }
+    }
+});
+
+
+fluid.defaults("flock.demo.oscilOscil.nexusui", {
+    gradeNames: "fluid.component",
+    listeners: {
+        onCreate: {
+            func: function(){
+                let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+                let height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+                width -=15;
+                let oscilloscope = new Nexus.Oscilloscope('#scope', {size: [width, 100]});
+                let spectrogram = new Nexus.Spectrogram('#spectogram', {size: [width, 100]})
+                oscilloscope.connect(flock.environment.audioSystem.nativeNodeManager.outputNode);
+                spectrogram.connect(flock.environment.audioSystem.nativeNodeManager.outputNode);
             }
         }
     }

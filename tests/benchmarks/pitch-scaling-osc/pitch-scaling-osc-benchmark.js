bdea01a5052b095c12f9a9010569171398873129
Colin Clark <colinbdclark@gmail.com>
2021-01-29 13:01:02 -0500

    Fixes benchmarks.

--- tests/benchmarks/pitch-scaling-osc/pitch-scaling-osc-benchmark.js
+++ tests/benchmarks/pitch-scaling-osc/pitch-scaling-osc-benchmark.js
@@ -1,16 +1,20 @@
 fluid.registerNamespace("flock.test");
 
 flock.test.pitchScalingOscBenchmark = function () {
+    var enviro = flock.init();
+
     sheep.test([
         {
             name: "flock.ugen.osc",
-            numReps: 1000000,
+            numReps: 100000,
             setup: function () {
                 return flock.synth({
-                    ugen: "flock.ugen.sinOsc",
-                    tableSize: 8192,
-                    freq: 440,
-                    mul: 1.0
+                    synthDef: {
+                        ugen: "flock.ugen.sinOsc",
+                        tableSize: 8192,
+                        freq: 440,
+                        mul: 1.0
+                    }
                 });
             },
             test: flock.evaluate.synth
@@ -20,14 +24,38 @@ flock.test.pitchScalingOscBenchmark = function () {
             numReps: 1000000,
             setup: function () {
                 return flock.synth({
-                    ugen: "flock.ugen.scalingOsc",
-                    table: flock.fillTable(8192, flock.tableGenerators.sin),
-                    tableSize: 8192,
-                    freq: 440,
-                    mul: 1.0
+                    synthDef: {
+                        ugen: "flock.ugen.scalingOsc",
+                        table: flock.fillTable(8192, flock.tableGenerators.sin),
+                        tableSize: 8192,
+                        freq: 440,
+                        mul: 1.0
+                    }
                 });
             },
             test: flock.evaluate.synth
+        },
+        {
+            name: "flock.ugen.scalingOsc with kr freqScale",
+            numReps: 1000000,
+            setup: function () {
+                return flock.synth({
+                    synthDef: {
+                        ugen: "flock.ugen.scalingOsc",
+                        table: flock.fillTable(8192, flock.tableGenerators.sin),
+                        tableSize: 8192,
+                        freq: 440,
+                        mul: 1.0,
+                        freqScale: {
+                            ugen: "flock.ugen.lfNoise",
+                            freq: 1,
+                            rate: "control"
+                        }
+                    }
+                });
+            },
+            test: flock.evaluate.synth
+
         }
     ])
 };
